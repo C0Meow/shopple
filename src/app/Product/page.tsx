837cc2e932d5d3ee4e498cb36e662457a8a2999e
CarlsonC09 <carlsonchuang@gmail.com>
2024-02-04 01:16:00 +0800

    looks very cool, gonna try combining it into my other project later

--- src/app/Product/page.tsx
+++ src/app/Product/page.tsx
@@ -5,13 +5,15 @@ import { Badge, Drawer, Grid } from "@mui/material";
 import { useState } from "react";
 import { api } from "~/trpc/react";
 import { CartItemType } from "../page";
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { Wrapper } from "./Item/item.styles";
 import * as React from 'react';
 import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
+import AddShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Item from "./Item/page";
+import { StyledButton } from "./product.styles";
+import Cart from "./Cart/page";
 
 function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
     return (
@@ -53,33 +55,54 @@ export default function StorePage(){
     const {data, isLoading, error} = useQuery<CartItemType[]>(["products"], getProducts);
     console.log(data);
 
-    const getTotalItems = () =>null;
-    const handleAddToCart = (clickedItem: CartItemType) =>null;
-    const handleRemoveFromCart = () =>null;
+    const [cartOpen, setCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
+    const getTotalItems = (items: CartItemType[]) =>
+      items.reduce((ack: number, item)=> ack + item.amount, 0)
+    ;
+    const handleAddToCart = (clickedItem: CartItemType) => {
+      setCartItems(prev => {
+        // 1. Is the item already added in the cart?
+        const isItemInCart = prev.find(item => item.id === clickedItem.id);
+  
+        if (isItemInCart) {
+          return prev.map(item =>
+            item.id === clickedItem.id
+              ? { ...item, amount: item.amount + 1 }
+              : item
+          );
+        }
+        // First time the item is added
+        return [...prev, { ...clickedItem, amount: 1 }];
+      });
+    };
+    const handleRemoveFromCart = (id: number) => {
+      setCartItems(prev =>
+        prev.reduce((ack, item) => {
+          if (item.id === id) {
+            if (item.amount === 1) return ack;
+            return [...ack, { ...item, amount: item.amount - 1 }];
+          } else {
+            return [...ack, item];
+          }
+        }, [] as CartItemType[])
+      );
+    };
 
     if (isLoading) return <LinearWithValueLabel />;
     if (error) return <div>something went wrong...</div>
-
-    // const user = useUser();
-    // const [input, setInput] = useState("");
-    // const ctx = api.useUtils();
-    // const {mutate, isLoading: isPosting} = api.orders.create.useMutation({onSuccess: () =>{
-    //     setInput("");
-    //     void ctx.orders.getAll.invalidate();
-    //     toast.success("留言成功! 請手動刷新去查看你最新的留言!");
-    // },
-    // onError: (e) => {
-    //     const errorMessage = e.data?.zodError?.fieldErrors.content;
-    //     if (errorMessage?.[0]){
-    //       toast.error(errorMessage[0]!);
-    //     }
-    //     else {
-    //       toast.error("留言失敗, 請稍候再嘗試");
-    //     }
-    // }});
     
     return (
       <Wrapper>
+        <Drawer anchor='right' open={cartOpen} onClose={()=>setCartOpen(false)}>
+          <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart}/>
+        </Drawer>
+        <StyledButton onClick={()=>setCartOpen(true)}>
+          <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+            <AddShoppingCartIcon />
+          </Badge>
+        </StyledButton>
         <Grid container spacing={3}>
             {data?.map(item => (
                 <Grid item key={item.id} xs={12} sm={4}>
